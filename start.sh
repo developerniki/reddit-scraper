7cb415ff5e6143b8f4fb652153f96672430982a3
developerniki <114883451+developerniki@users.noreply.github.com>
2022-12-24 20:05:48 +0100

    Add `update_missing_flair_posts.py` to `start.sh`

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 md5f1=$(md5sum 'data/posts.json' | cut -d' ' -f1)
 .venv/bin/python fetch_posts.py
+.venv/bin/python update_missing_flair_posts.py
 md5f2=$(md5sum 'data/posts.json' | cut -d' ' -f1)
 
 if [ "$md5f2" != "$md5f1" ]; then
