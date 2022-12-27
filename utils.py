37064cf93635d5b85b785c97b2ad6f9735499693
developerniki <114883451+developerniki@users.noreply.github.com>
2022-12-27 19:43:05 +0100

    Improve docstrings

--- utils.py
+++ utils.py
@@ -6,14 +6,46 @@ from typing import Dict
 
 
 def extract_markdown_url(text: str) -> str:
-    """Extract the first URL from a Markdown string."""
+    """Extract the first URL from a Markdown string.
+
+    Parameters:
+        text (str): The Markdown string to extract the URL from.
+
+    Returns:
+        str: The first URL found in the string, or `None` if no URL was found.
+    """
     url = re.search(r'\[.+\]\((?P<url>https?://\S+)\)', text)
     url = None if url is None else url.group('url')
     return url
 
 
-def extract_article_summary(comments: Dict) -> str:
-    """Use depth first search to concatenate uninterrupted submitter comment/reply chain."""
+def extract_article_summary(comments: List[Dict]) -> str:
+    """Extract the chain of comments and replies made by the submitter by traversing the hierarchical structure of
+    comments and replies using depth first search, until a comment or reply from another user is encountered.
+
+    Parameters:
+        comments: A list of dictionaries of comments and replies. Each dictionary has the following structure:
+            ```
+            {
+                'is_submitter': bool,
+                'body': str,
+                'replies': [
+                    {
+                        'is_submitter': bool,
+                        'body': str,
+                        'replies': [
+                            {...},
+                            ...
+                        ]
+                    },
+                    ...
+                ]
+            }
+            ```
+
+    Returns:
+        A string containing the concatenated comment/reply chain of the submitter, separated by '\n\n∴\n\n'.
+    """
     summary = []
     queue = deque(comments)
     while queue:
